ed74588a27533f228b08091d20bc5b2fdd8cdb43
Hpar ionic <hpavavar@gmail.com>
2015-03-23 18:19:21 +0100

    +login

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,32 @@
 'use strict';
 
-angular.module('prodapps', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap','buche'])
+angular.module('prodapps', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap','buche', 'odoo'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'app/main/main.html',
+        templateUrl: 'app/main/mmain.html',
         controller: 'MainCtrl'
       })
       .state('login', {
         url: '/login',
         templateUrl: 'app/login/login.html',
         controller: 'LoginCtrl'
-      });
-
+	});
     $urlRouterProvider.otherwise('/');
+	})
+.run(function ($rootScope, $cookies, $state) {
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+		if (toState.name === 'login')
+			return;
+console.log($cookies.session_id) 
+		//if (jsonRpc.isLoggedin()) {
+		if (!$cookies.session_id) {
+			console.log('not logged in');
+			event.preventDefault();
+			$state.go('login');
+		}
+	}); 
+
   })
 ;
--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,9 +1,22 @@
 'use strict';
 
 angular.module('prodapps')
-  .controller('LoginCtrl', function ($scope) {
+  .controller('LoginCtrl', function ($scope,$state, jsonRpc, $cookies) {
     $scope.login = function () {
-      console.log($scope.bucheUsername);
-      console.log($scope.buchePassword);
-    }
+	$scope.error = "";
+	jsonRpc.login('db',$scope.bucheUsername,$scope.buchePassword).then(function () {
+		console.log('login succeed');
+		$state.go('home');
+	}, function () {
+		$scope.error = "Authentication failed";
+	//	$cookies.session_id='';
+
+	});
+    };
+	$scope.logout = function () {
+		$cookies.session_id='';
+	//	jsonRpc.logout();
+		$state.go('login');
+};
+	$scope.logout();
   })
