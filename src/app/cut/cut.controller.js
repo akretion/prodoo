c135d4549e4bc2b49554eaeb5e382b8d212965b4
Hpar ionic <hpavavar@gmail.com>
2015-03-26 17:45:33 +0100

    work on cut

--- src/app/cut/cut.controller.js
+++ src/app/cut/cut.controller.js
@@ -4,29 +4,42 @@ angular.module('prodapps')
   .controller('CutCtrl', function ($scope, $state, jsonRpc, $cookies) {
   console.log('Cut ctrl');
 
+	$scope.workcenter = $state.params.workcenter;
   $scope.list = [];
-	$scope.list = [{'status': 'toDo', 'id': 'a', 'title': 'Couper un tronc', 'long': '<p>Prendre un bon gros tronc de bucheron</p><p><img src="http://www.mercialfred.com/media-data/864/photo_article_864.jpg"></p>'},
-				   {'status': 'toDo', 'id': 'b', 'title': 'Enlever les feuilles', 'long': 'On va pas attendre l\'autommne !<br><img src="http://www.nantes-shiatsu.fr/blog/wp-content/uploads/2008/09/automne.jpg">'}];
-	$scope.current = { filter: { 'status':'toDo'},  item : $scope.list[0]};
+	$scope.current = { filter: { 'state':'draft'},  item : {sequence: 99999}}; 
   	$scope.fetchList = function () {
   		console.log('fetchList');
   		
         $scope.list = jsonRpc.syncImportObject({
             model: 'mrp.production.workcenter.line',
             func_key: 'prodoo',
-            domain: [],
+            domain: [['workcenter_id', '=', $scope.workcenter ]],
             limit: 50,
-            interval: 1000
+            interval: 2000
         });
+	$scope.$watch('list.timekey', function (newVal, oldVal) {
+		console.log('watched !', oldVal, newVal);
+		$scope.data = [];
+		var item;
+		for(var key in $scope.list.data) {
+			item = $scope.list.data[key];
+			if (item.sequence < $scope.current.item.sequence && item.state != 'done')
+				$scope.current.item = item;
+
+			$scope.data.push(item);
+		}
+	});
+	
     console.log($scope.list);
     }      
   	$scope.fetchList();
+
 
   	$scope.print = function (item) {
   		console.log('print ! ', item);
   	};
 
   	$scope.do = function (item) {
-  		item.status = 'done';
-  	}
+  		item.state= 'done';
+  	};
   });
