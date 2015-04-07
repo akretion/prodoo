ac11ab2dd1618546f2fafe98f965a2c538f0c053
Hpar ionic <raph@hpar.fr>
2015-04-07 11:28:47 +0200

    prodooSync sur decoupe

--- src/app/decoupe/decoupe.controller.js
+++ src/app/decoupe/decoupe.controller.js
@@ -1,27 +1,30 @@
 'use strict';
 
 angular.module('prodapps')
-  .controller('DecoupeCtrl', function ($scope, $state, jsonRpc) {
-
-	$scope.workcenter = ;
+  .controller('DecoupeCtrl', function ($scope, $state, jsonRpc, prodooSync) {
 	$scope.modale = { casier : '' };
 	$scope.current = { filter: { 'state':'draft'},  item : {sequence: 99999}}; 
-  
-  destroy = prodooSync({domain: $state.params.workcenter}, $scope.data, $scope.current);
+ 	console.log('prodooSync', prodooSync); 
+	$scope.sync = { data: null , current: $scope.current};
+  var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
 
-  $scope.$on('$destroy', destroy);
+  $scope.$on('$destroy', function() {
+	console.log('destroy', destroy);
+	destroy();
+});
 
-
+$scope.$watch('sync.data', function () {
+	console.log('data changed', $scope.sync);
+});
   
   $scope.print = function (item) {
   		console.log('print ! ', item);
   };
 
-  	$scope.do = function (item, modale) {
-		console.log('modale ', modale);
-        	jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, modale.casier]).then(function () {
+  	$scope.do = function (item ) {
+        	jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id]).then(function () {
+			item.state = 'done';
 		}, function () {
 		});
-		modale.casier = "";
   	};
   });
