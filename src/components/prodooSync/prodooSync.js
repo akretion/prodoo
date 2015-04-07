d10a3cf01af266dbd960f00f9f0c89f3eb28932a
Hpar ionic <raph@hpar.fr>
2015-04-07 11:28:35 +0200

    ameliorations prodooSync

--- src/components/prodooSync/prodooSync.js
+++ src/components/prodooSync/prodooSync.js
@@ -1,39 +1,41 @@
 'use strict';
 
-angular.module('prodapps').provider('prodooSync', ['prodooConfig', 'jsonRpc', function prodooSyncProvider(prodooConfig, jsonRpc) {
+angular.module('prodapps').provider('prodooSync', [function prodooSyncProvider() {
 
 	var sync = {};
 	sync.liste = {};
 
-	sync.syncData = function(options, objectRef, current) {
+this.$get = ['jsonRpc', 'prodooConfig', function (jsonRpc, prodooConfig) {
+	sync.syncData = function(options, objectRef) {
+	console.log('options : ', options, objectRef);	
+ 	sync.liste = jsonRpc.syncImportObject({
+		model: 'mrp.production.workcenter.line',
+		func_key: 'prodoo',
+		domain: [['workcenter_id', '=', options.workcenter ]],
+		limit: 50,
+		interval: prodooConfig.refreshInterval,
+	});
 
- 		sync.liste = jsonRpc.syncImportObject({
-			model: 'mrp.production.workcenter.line',
-			func_key: 'prodoo',
-			domain: options.domain,
-			limit: 50,
-			interval: prodooConfig.refreshInterval,
-		});
+ 	sync.liste.watch(function (e) {
+		console.log(sync.liste.data, objectRef);
+		//objectRef.data.splice(0, objectRef.data.length);
+		objectRef.data = [];
+		var item, key;
+		for(key in sync.liste.data) {
+			item = sync.liste.data[key];
+			if (item.sequence < objectRef.current.item.sequence && item.state != 'done')
+				objectRef.current.item = item;
 
- 		sync.liste.watch(function (e) {
-			console.log('on ma updated',e);
-			objectRef = [];
-			var item, key;
-			for(key in sync.liste.data) {
-				item = sync.liste.data[key];
-				if (item.sequence < current.item.sequence && item.state != 'done')
-					current.item = item;
+			objectRef.data.push(item);
+		}
+	});
 
-				objectRef.push(item);
-		});
-
-		return sync.liste.stopCallback;
+	return sync.liste.stopCallback;
  	};
 
 
 
-	this.$get = ['jsonRpc', function (jsonRpc) {
-			return sync;
+	return sync;
 	}];
 
 	return this;
