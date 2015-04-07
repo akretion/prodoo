471f04fd52eb10afc12653b644087267427bdca0
Hpar ionic <raph@hpar.fr>
2015-04-07 15:03:46 +0200

    +douchette

--- src/app/cut/cut.controller.js
+++ src/app/cut/cut.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('prodapps')
 	.controller('CutCtrl', function ($scope, $state, jsonRpc, prodooSync, $notification) {
-
+	$scope.casier = [];
 	$scope.sync = { data: null, current: { filter: { 'state':'draft'},  item : {sequence: 99999}}};
 	var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
 
@@ -11,19 +11,13 @@ angular.module('prodapps')
 		$notification('Printing...');
 	};
 
-	$scope.do = function (item, modale) {
+	$scope.do = function (item) {
 		$scope.print(item);
-		$scope.markAsDone(item, modale);
+		$scope.markAsDone(item);
 	}
 
-	$scope.markAsDone = function (item, modale) {
-		var casier = null;
-		if (modale && modale.casier)
-			casier = modale.casier;
-
-			jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, casier]).then(function () {
-			if (casier)
-				modale.casier = "";
+	$scope.markAsDone = function (item) {
+		jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, $scope.casier.join(';')]).then(function () {
 			item.state = 'done';
 			$notification('Done');
 		}, function () {
