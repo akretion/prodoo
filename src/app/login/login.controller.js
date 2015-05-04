b474862cd8c65805828b7ca57bc4c7ee5d26ce22
raph <hpavavar@gmail.com>
2015-05-04 14:23:22 +0200

    formating login.js

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,20 +1,20 @@
 'use strict';
 
 angular.module('prodapps')
-  .controller('LoginCtrl', function ($scope,$state, jsonRpc, prodooConfig) {
-    $scope.login = function () {
-	$scope.error = "";
-	jsonRpc.login(prodooConfig.db,$scope.bucheUsername,$scope.buchePassword).then(function () {
-		console.log('login succeed');
-		$state.go('home.main');
-	}, function () {
-		$scope.error = "Authentication failed";
-	});
-    };
+.controller('LoginCtrl', function ($scope,$state, jsonRpc, prodooConfig) {
+	$scope.login = function () {
+		$scope.error = "";
+		jsonRpc.login(prodooConfig.db,$scope.bucheUsername,$scope.buchePassword).then(function () {
+			console.log('login succeed');
+			$state.go('main.home');
+		}, function () {
+			$scope.error = "Authentication failed";
+		});
+	};
 	$scope.logout = function () {
 		console.log('logout');
 		jsonRpc.logout();
 		$state.go('login');
 	};
 	$scope.logout();
-  })
+});
