0f56f1c9a8e2b0e085c660e3747a7b191aef81c3
raph <hpavavar@gmail.com>
2015-05-07 18:09:04 +0200

    try to fix bug in venetian

--- src/app/venetianAssembly/venetianAssembly.controller.js
+++ src/app/venetianAssembly/venetianAssembly.controller.js
@@ -42,7 +42,7 @@ angular.module('prodapps')
 		});
 
 		$scope.salesDone = $scope.sync.data.filter(function (e) {
-			return draft.indexOf(e.sale_name) === -1;
+			return draft.indexOf(e.sale_name) === -1 && $scope.sync.current.item.sale_name != e.sale_name;
 		});
 		console.log(draft, $scope.salesDone);
 	};
