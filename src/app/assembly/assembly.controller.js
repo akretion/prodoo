855a8129a18211439bd0ed1b29a64858a2aafe56
raph <hpavavar@gmail.com>
2015-05-07 14:22:10 +0200

    assembly : casiers -> casier and init when changing current;item

--- src/app/assembly/assembly.controller.js
+++ src/app/assembly/assembly.controller.js
@@ -2,17 +2,18 @@
 
 angular.module('prodapps')
 .controller('AssemblyCtrl', function ($scope, $state, jsonRpc, prodooSync, $notification, prodooPrint) {
-	$scope.casier = [];
 	$scope.search = [];
 	$scope.sync = { data: null, current: { filter: { 'state':'draft'}}};
 	var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
 
 	$scope.fields = ["Tube", "Tissu"];
-	$scope.lines = 2;
 	$scope.scans = [
 		[null, null]
 		,[null, null]
 	];
-	$scope.casiers = [];
+	$scope.casier = [];
 	
+	$scope.$watch('sync.current.item', function () {
+		$scope.casier = []; //init casier
+	});
 });
