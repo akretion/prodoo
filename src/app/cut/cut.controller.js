83d085c51a574e21c632e563714ef982526d816f
raph <hpavavar@gmail.com>
2015-05-07 16:23:07 +0200

    casier => rack in cut and strict

--- src/app/cut/cut.controller.js
+++ src/app/cut/cut.controller.js
@@ -7,12 +7,12 @@ angular.module('prodapps')
 	var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
 	$scope.prefillCasier = function (item) {
 		//prefill casier array with casier comming from samed order
-		$scope.casier = [];
+		$scope.rack = [];
 		$scope.sync.data.filter(function (i) {
 			return i.lot_number === item.lot_number;
 		}).forEach(function (item) {
-			if ($scope.casier.indexOf(item.rack) === -1)
-				$scope.casier.push(item.rack);
+			if ($scope.rack.indexOf(item.rack) === -1)
+				$scope.rack.push(item.rack);
 		});
 	};
 
@@ -28,7 +28,7 @@ angular.module('prodapps')
 	};
 
 	$scope.markAsDone = function (item) {
-		jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, $scope.casier.join(';')]).then(function () {
+		jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, $scope.rack.join(';')]).then(function () {
 			item.state = 'done';
 			$notification('Done');
 		}, function () {
--- src/app/stripCut/stripCut.controller.js
+++ src/app/stripCut/stripCut.controller.js
@@ -7,8 +7,18 @@ angular.module('prodapps')
 	$scope.sync = { data: null, current: { filter: { 'state':'draft'}}};
 	var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
 
+	$scope.prefillCasier = function (item) {
+		//prefill casier array with casier comming from samed order
+		$scope.rack = [];
+		$scope.sync.data.filter(function (i) {
+			return i.lot_number === item.lot_number;
+		}).forEach(function (item) {
+			if ($scope.rack.indexOf(item.rack) === -1)
+				$scope.rack.push(item.rack);
+		});
+	};
+
 	$scope.print = function (item) {
-		console.log('print ! ', item);
 		prodooPrint(item);
 		$notification('Printing...');
 	};
@@ -19,7 +29,7 @@ angular.module('prodapps')
 	}
 
 	$scope.markAsDone = function (item) {
-		jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, $scope.casier.join(';')]).then(function () {
+		jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, $scope.rack.join(';')]).then(function () {
 			item.state = 'done';
 			$notification('Done');
 		}, function () {
