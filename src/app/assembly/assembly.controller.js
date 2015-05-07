2d7cb69b9d3c2bc20585f0b4cb09ac3217924cde
raph <hpavavar@gmail.com>
2015-05-07 14:56:09 +0200

    add relatives components in assembly.controller

--- src/app/assembly/assembly.controller.js
+++ src/app/assembly/assembly.controller.js
@@ -2,18 +2,25 @@
 
 angular.module('prodapps')
 .controller('AssemblyCtrl', function ($scope, $state, jsonRpc, prodooSync, $notification, prodooPrint) {
-	$scope.search = [];
 	$scope.sync = { data: null, current: { filter: { 'state':'draft'}}};
 	var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
-
-	$scope.fields = ["Tube", "Tissu"];
-	$scope.scans = [
-		[null, null]
-		,[null, null]
-	];
+	$scope.fields = [];
+	$scope.scans = [];
 	$scope.casier = [];
 	
-	$scope.$watch('sync.current.item', function () {
+	$scope.$watch('sync.current.item', function (newVal) {
+		if (!newVal)
+			return;
 		$scope.casier = []; //init casier
+
+		$scope.fields = newVal.components;
+		$scope.scans = [];
+		var line = [], k = 0;
+		for (k = 0; k < newVal.components.length; k++)
+			line.push(null);
+
+		for (k = 0; k < newVal.qty; k++)
+			$scope.scans.push(line)
+
 	});
 });
