bde8a226ee073dd23f3dc189e3f181da405b01bc
Hpar ionic <raph@hpar.fr>
2015-05-28 13:24:29 +0200

    keep in memory line already validated and be able to change between tasks in assembly

--- src/app/assembly/assembly.controller.js
+++ src/app/assembly/assembly.controller.js
@@ -11,20 +11,25 @@ angular.module('prodapps')
     $scope.$watch('sync.current.item', function (newVal) {
         if (!newVal)
             return;
-        $scope.casier = []; //init casier
 
         $scope.fields = newVal.components;
-        $scope.scans = [];
-        //if item.components is [ {name: 'tissu'}, { name:'profile'}]
-        // and item.qty = 2
-        // then scans whould be [ [null, null], [null, null]]
-        // (Array.prototype.fill() is not ready yet / polyfill instead :
-        var line = [], k = 0;
-        for (k = 0; k < newVal.components.length; k++)
-            line.push(null);
 
-        for (k = 0; k < newVal.qty; k++)
-            $scope.scans.push(line)
+        if (!newVal.casiers)
+          newVal.casiers = [];
+
+        if (!newVal.scans) {
+          newVal.scans = [];
+          //if item.components is [ {name: 'tissu'}, { name:'profile'}]
+          // and item.qty = 2
+          // then scans whould be [ [null, null], [null, null]]
+          // (Array.prototype.fill() is not ready yet / polyfill instead :
+          var line = [], k = 0;
+          for (k = 0; k < newVal.components.length; k++)
+              line.push(null);
+
+          for (k = 0; k < newVal.qty; k++)
+              newVal.scans.push(line);
+        }
 
     });
 
