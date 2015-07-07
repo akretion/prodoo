akretion/prodoo

commit d3fb980e8fb9d36baf3f94a73ed1adfcb606aaf3

refactoring of forms in assembly cleaner design, cleaner html

src/app/assembly/assembly.controller.js
@@ -5,6 +5,7 @@ angular.module('prodapps')
     $scope.sync = { data: null, current: { filter: { 'state':'!done'}}};
     var destroy = prodooSync.syncData({workcenter: $state.params.workcenter}, $scope.sync);
     $scope.fields = [];
+    $scope.sameLotNumber = [];
 
     $scope.$watch('sync.current.item', function (newVal) {
         if (!newVal)
@@ -13,51 +14,75 @@ angular.module('prodapps')
         newVal._v = newVal._v || {};
 
         $scope.fields = newVal.components;
+        $scope.sameLotNumber = $scope.sync.data.filter(function (i) {
+          return i.lot_number === newVal.lot_number && i.id != newVal.id;
+        });
+
+        //some values needs to be calculated once, because the user may
+        //start to fill the form
+        //go to another task (complete it)
+        //go back to the first task (and we don't want to loose any data)
+
+        //do it each time because task with sameLotNumber may be completed
+        newVal._v.suggestedRacks = unserializeRacks( ($scope.sameLotNumber[0]) ? ($scope.sameLotNumber[0].rack[0]) : null);
+
+        if (!newVal._v.racks) //do it only once (because user may have entered some data)
+          newVal._v.racks = unserializeRacks(newVal.rack[0]);
         
-        newVal._v.casiers = []; //item.rack is a semicol separated list; we want it as an array (.length = qty)
-        if (newVal.rack[0])
-          newVal._v.casiers = newVal.rack[0].split(';') //[]; //rack shoud be a better fit !
+        if (!newVal._v.scans) //do it only once (because user may have entered some date)
+          newVal._v.scans = createArray(newVal.qty).map(function () {
+            //if item.components is [ {name: 'tissu'}, { name:'profile'}]
+            // and item.qty = 2
+            // then scans whould be [ [null, null], [null, null]]
 
-        if (!newVal._v.scans) {
-        //for storing the scans - only usefull for the operator for keeping track of progression
-        //and ensuring she selected the good input product
-        //scan is not stored in odoo
+            return createArray(newVal.components.length);
+          });
+      
+        if (!newVal._v.locks) //do it only once (because some lines may be terminated )
+          newVal._v.locks = createArray(newVal.qty);
 
-          newVal._v.scans = [];
-          newVal._v.locks = [];
-          //if item.components is [ {name: 'tissu'}, { name:'profile'}]
-          // and item.qty = 2
-          // then scans whould be [ [null, null], [null, null]]
-          // (Array.prototype.fill() is not ready yet / polyfill instead :
+        //do it each time
+        newVal._v.lines = createArray(newVal.qty).map(function (unused, idx) {
+          var l = {};
+          //for storing the scans - only usefull for the operator for keeping track of progression
+          //and ensuring she selected the good input product
+          l.scans = newVal._v.scans[idx];
 
-          var line = [], k = 0, l = 0;
-          for (k = 0; k < newVal.qty; k++) {
-            line = [];
-            for (l = 0; l < newVal.components.length; l++) {
-              line.push(null);
-            }
-            newVal._v.scans.push(line);
-            newVal._v.locks.push(true); //for locking the lines
+          //for storing the output rack of the operation 
+          //it may be already filled  
+          //stored in odoo
+          l.rack = newVal._v.racks[idx];
+
+          //it's may be already filled
+          l.suggestedRack = newVal._v.suggestedRacks[idx];
+          
+          //for locking the line when it's filled and valid
+          l.lock = newVal._v.locks[idx];
+          return l;
+        });
+
+
+
+        function createArray(length) {
+        //create and fill with null an array of length
+        // (Array.prototype.fill() is not ready yet / polyfill instead :
+          var a = [], l = 0;
+          for (l = 0; l < length; l++) {
+            a.push(null);
           }
+          return a;
         }
 
-        //if there is another task with same lot_number
-        //and the other one is done
-        //and they have the same qty
-        //then we can prefill "suggestedRack"  
-        newVal._v.suggestedRack = [];
+        function unserializeRacks(racks) {
+          //currently item.rack is a kind of : ";;a;b" instead of ['a','b']
 
-        $scope.sync.data.filter(function (i) {
-          return i.lot_number === newVal.lot_number && i.id != newVal.id;
-        }).forEach(function (item) { //normalement on devrait en avoir qu'un
-          //should be : 
-          // newVal.suggestedRack = item.rack;
-          //but currently item.rack is a kind of : ";;a;b" instead of ['a','b']
-          if (item.rack[0])
-            newVal._v.suggestedRack = item.rack[0] //because it's a [string]
-            .split(';') //';' is the current separator
-            .filter(function (i) { return i.length; }); //trim shit
-        });
+          if (!racks)
+            return [];
+          console.log('racks',racks);
+          return racks.split(';').filter(function (i) { return i.length; }); //trim shit with filter
+        }
+
+
     });
 
     $scope.clickTask = function (item) {

src/app/assembly/assembly.html
@@ -76,10 +76,15 @@
                 <th>Suggested rack</th>
                 <th>Rack</th>
             </tr>
-            <tbody ng-repeat="scan in sync.current.item._v.scans track by $index" ng-form="f{{$index}}">
-                <tr ng-hide="grid['f'+$index].$valid && sync.current.item._v.locks[$index]">
-                    <td ng-repeat="s in scan track by $index">
-                      <input class="haute-ligne" ng-model="sync.current.item._v.scans[$parent.$index][$index]" required="true" ensure-value="{{ sync.current.item.lot_number }}" ng-focus="sync.current.item._v.scans[$parent.$index][$index]=''; sync.current.item._v.locks[$parent.$index]=false" ng-blur="sync.current.item._v.locks[$parent.$index]=grid['f'+$parent.$index].$valid"></input>
+            <tbody ng-repeat="line in sync.current.item._v.lines track by $index" ng-form="f{{$index}}">
+                <tr ng-hide="grid['f'+$index].$valid && line.lock">
+                    <td ng-repeat="s in line.scans track by $index">
+                      <input class="haute-ligne" required="true" 
+                        ng-model="line.scans[$index]"
+                        ng-focus="line.scans[$index]=''; line.lock=false" 
+                        ng-blur="line.lock=grid['f'+$parent.$index].$valid"
+                        ensure-value="{{ sync.current.item.lot_number }}">
+                      </input>
                     </td>
                     <td>
                         <div class="btn-group">
@@ -88,19 +93,28 @@
                     </td>
                     <td>
                         <div class="btn-group">
-                            <button type="button" class="btn btn-warning haute-ligne" ng-click="print(sync.current.item)" ng-disabled="!sync.current.item.label" ><span class="glyphicon glyphicon-print"></span></button>
+                            <button type="button" class="btn btn-warning haute-ligne" 
+                            ng-click="print(sync.current.item)" 
+                            ng-disabled="!sync.current.item.label"><span class="glyphicon glyphicon-print"></span></button>
                         </div>
                     </td>
                     <td>
-                        {{ sync.current.item._v.suggestedRack[$index] }}
+                        {{ line.suggestedRack }}
                     </td>
-                        <td><input class="haute-ligne col-xs-5" ng-model="sync.current.item._v.casiers[$index]" required="true" ng-focus="sync.current.item._v.casiers[$index]=''; sync.current.item._v.locks[$index]=false" ng-blur="sync.current.item._v.locks[$index]=grid['f'+$index].$valid"></input>
-                        <button class="btn pull-right" ng-click="sync.current.item._v.locks[$index]=true" ng-show="grid['f'+$index].$valid">Edit</button>
+                        <td>
+                          <input class="haute-ligne col-xs-5" required="true"
+                            ng-model="line.rack"
+                            ng-focus="line.rack=''; line.lock=false" 
+                            ng-blur="line.lock=grid['f'+$index].$valid">
+                           </input>
+                        <button class="btn pull-right" 
+                        ng-click="line.lock=true" 
+                        ng-show="grid['f'+$index].$valid">Edit</button>
                     </td>
 
                 </tr>
-                <tr ng-show="grid['f'+$index].$valid && sync.current.item._v.locks[$index]">
-                  <td ng-repeat="s in scan track by $index">
+                <tr ng-show="grid['f'+$index].$valid && line.lock">
+                  <td ng-repeat="s in line.scans track by $index">
                     {{ s }}
                   </td>
                     <td>
@@ -110,11 +124,13 @@
                     </td>
                     <td>
                         <div class="btn-group">
-                            <button type="button" class="btn btn-warning haute-ligne" ng-click="print(sync.current.item)"  ng-disabled="!sync.current.item.label"><span class="glyphicon glyphicon-print"></span></button>
+                            <button type="button" class="btn btn-warning haute-ligne" 
+                            ng-click="print(sync.current.item)"
+                            ng-disabled="!sync.current.item.label"><span class="glyphicon glyphicon-print"></span></button>
                         </div>
                     </td>
                     <td></td>
-                    <td>{{sync.current.item._v.casiers[$index]}} <button class="btn pull-right" ng-click="sync.current.item._v.locks[$index]=false">Edit</button></td>
+                    <td>{{line.rack}} <button class="btn pull-right" ng-click="line.lock=false">Edit</button></td>
                 </tr>
             </tbody>
         </table>
@@ -129,15 +145,16 @@
         <div ng-hide="sync.current.item.state == 'done'">
             <div class="btn-group btn-group-justified">
                 <div class="btn-group">
-                    <button ng-disabled="grid.$invalid || sync.current.item.pending || sync.current.item._v.lock" class="btn btn-success" data-toggle="modal" data-target="#casier-modal" ng-click="do(sync.current.item)"> OK</button>
+                    <button class="btn btn-success" 
+                    ng-disabled="grid.$invalid || sync.current.item.pending || sync.current.item._v.lock" 
+                    ng-click="do(sync.current.item)"
+                    data-toggle="modal" data-target="#casier-modal"> OK</button>
                 </div>
             </div>
         </div>
 
         </form>
-        </ion-scroll>
-
-
+      </ion-scroll>
     </div>
 </div>
 </div>

commit 8f45b5ce2160d8329ecbdf8b48f5756908e32446

fix regression introduced in d3fb980 (rack not transmited)

src/app/assembly/assembly.controller.js
@@ -129,8 +129,10 @@ angular.module('prodapps')
       $notification('Pending');
       item._v.lock = true;
 
-      if (item._v.casiers && item._v.casiers.length > 0 )
-        item.rack = item._v.casiers.join(';');
+      //serialize racks
+      item.rack = item._v.lines.map(function (r) {
+        return r.rack; 
+      }).join(';');
 
       jsonRpc.call('mrp.production.workcenter.line', 'prodoo_action_done', [item.id, item.rack ]).then(function () {
         item.state = 'done';