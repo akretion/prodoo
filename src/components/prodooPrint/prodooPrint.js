fa924c07ef07dc3482f090d388684072579bab18
hparfr <raph@hpar.fr>
2015-12-18 10:14:52 +0100

    Beep in prodoo{Machine||Print}

--- src/components/prodooPrint/prodooPrint.js
+++ src/components/prodooPrint/prodooPrint.js
@@ -2,12 +2,13 @@
 
 angular.module('prodapps').provider('prodooPrint',[ function prodooPrintProvider() {
 
-    this.$get = ['$http', 'prodooConfig', function ($http, prodooConfig) {
+    this.$get = ['$http', 'prodooConfig', 'Beep', function ($http, prodooConfig, Beep) {
             return function (payload, qte) {
                 var req = {
                     args: ['label', payload.label],
                     kwargs: { options : { 'copies': (qte) ? qte : payload.quantities }}
                 };
+                Beep.play();
                 $http.post(prodooConfig.pyWebDriver+'/cups/printData', req);
                 console.log('print !!!!', payload);
             };
