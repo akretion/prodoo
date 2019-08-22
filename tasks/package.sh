dab6480b049172472d059491cace2cb305448ccb
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2019-08-22 13:01:32 +0200

    When running package task, before main action delete old image

--- tasks/package.sh
+++ tasks/package.sh
@@ -28,6 +28,7 @@ Step "Login to AWS ECR"
 eval `aws ecr get-login --profile ostore-registry-reader --region eu-west-1 --no-include-email`
 
 Step "Build the docker image"
+docker rmi ${GPS_PROJECT_DOCKER_IMAGE_URL}:latest
 docker build --no-cache --force-rm  -f ${GPS_PROJECT_DIR}/etc/docker/Dockerfile  ${_BUILD_ARGS_OPTS}  -t ${GPS_PROJECT_DOCKER_IMAGE_URL} ${GPS_PROJECT_DIR}
 
 Check_errors $?
