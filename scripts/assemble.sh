1f1412ff8fc3af45cde8582d0a682fa05c961de3
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2019-08-22 14:00:44 +0200

    small fix

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -13,7 +13,7 @@ cp $OS_BUILD/etc/config/prodoo/build_package.json ./package.json
 
 # copy prodoo config file
 gomplate --file $OS_BUILD/etc/config/prodoo/prodooConfig.js.dist.gomplate --out $OS_BUILD/etc/config/prodoo/prodooConfig.js.dist
-cp $OS_BUILD/etc/config/prodoo/prodooConfig.js.dist .src/components/prodooConfig/prodooConfig.js
+cp $OS_BUILD/etc/config/prodoo/prodooConfig.js.dist $OS_BUILD/src/src/components/prodooConfig/prodooConfig.js
 rm -rf $OS_BUILD/etc/config/prodoo/prodooConfig.js.dist
 
 # install all node dep
@@ -30,4 +30,4 @@ mv ./gulpfile.js ./gulpfile.js.old
 cp $OS_BUILD/etc/config/gulp/gulpfile.js ./gulpfile.js
 
 # build app
-gulp build
+gulp build --verbose
