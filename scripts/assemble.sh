fc5328ac31bae53e8b0ed580f0ef403f5172b149
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2021-01-19 13:24:40 +0100

    what was changed: - totally removed bower - libs now are stored in assets/libs folder as static files - libs are now integral part of the project - npm is only now for istalling gulp - building is made by gulp (modified process adjust to new schema)
    what need to be done:
    - minimize vendor files
    - update gulp
    - update libs
    - delete old files

--- scripts/assemble.sh
+++ scripts/assemble.sh
@@ -7,22 +7,11 @@ source $OS_EXTRAS/files/bash/helpers.bash
 
 cd $OS_BUILD/src
 
-# set pathed package.json file as a primary one, original file will be backup
-mv ./package.json ./package.json.backup
-cp $OS_BUILD/etc/config/prodoo/build_package.json ./package.json
-
 # install all node dep
 npm install
 
 # add path to exec
 PATH=$PATH:./node_modules/.bin/
-
-# install all bower components
-bower install --allow-root
-
-# copy new gulp file
-mv ./gulpfile.js ./gulpfile.js.old
-cp $OS_BUILD/etc/config/gulp/gulpfile.js ./gulpfile.js
 
 # build app
 gulp build --verbose
