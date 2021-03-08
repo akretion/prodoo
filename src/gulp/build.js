de59bb05c8439e15ce5df854f82172a5c825ffbf
Marcin Wojtysiak <m.wojtysiak@oliverstore.com>
2021-03-08 13:09:56 +0100

    Fix build process

--- src/gulp/build.js
+++ src/gulp/build.js
@@ -49,7 +49,7 @@ gulp.task('html', ['inject', 'partials'], function () {
   }))
   .pipe(jsFilter.restore)
   .pipe(cssFilter)
-  .pipe($.replace('../assets/libs/bootstrap-sass-official_3.3.3/fonts/', '../fonts/'))
+  .pipe($.replace('../assets/libs/bootstrap-sass-official_3.3.3/fonts/bootstrap/', '../fonts/'))
   .pipe($.csso())
   .pipe(cssFilter.restore)
   .pipe(assets.restore())
