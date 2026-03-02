d83dc65061d03868da55fbf455dd52bec2ee661c
hparfr <raphael.reverdy@akretion.com>
2026-03-02 14:52:44 +0100

    Small improvements (remove console.log, add comments, ...)

--- prodoo/static/src/app/team/team.controller.js
+++ prodoo/static/src/app/team/team.controller.js
@@ -13,11 +13,10 @@ angular.element('#loginPassword').trigger('focus');
  refreshTeam();
 
  $scope.addMember = function(loginPassword) {
- 	//input is the same bardcode than login: part is username, part is password
- 	//we should not diplay password
- 	//see login.controller.js
- 	console.log('dans addMember', loginPassword)
-	var seperator = ' '
+    //input is the same bardcode than login: part is username, part is password
+    //we should not diplay password
+    //see login.controller.js
+    var seperator = ' '
     if (!loginPassword)
     	return;
     if (loginPassword.indexOf(seperator) != -1 ) {
@@ -48,18 +47,17 @@ angular.element('#loginPassword').trigger('focus');
     $notification('Saving...');
 
     teamProvider.set($scope.tempTeam).then(
-    	function( x) {
-    		console.log('tout c bine passe');
-    		refreshTeam();
-    		$scope.tempTeam = [];
-    	}, function( y) {
-    		console.log('shit happens');
-    	});
+        function(x) {
+            refreshTeam();
+            $scope.tempTeam = [];
+        }, function( y) {
+            console.log('shit happens', y);
+        });
  }
 
  function refreshTeam() {
- 	console.log('on va chercher la liste sur odoo');
- 	$scope.actualTeam = teamProvider.members;
+    // fetch team from odoo
+    $scope.actualTeam = teamProvider.members;
  }
 }]);
 
@@ -69,33 +67,25 @@ angular.module('prodapps').provider('teamProvider', [ function prodooConfigProvi
     var team = {
     	get: null,
     	set: null,
-    	members: ['w', 'y', 'zzz'],
+    	members: [],
     }
-    console.log('team provider', this)
     this.$get = ['jsonRpc', function (jsonRpc) {
-    	team.get = function() {
-    		//fetch the list from odoo
-    		return jsonRpc.call('prodoo_team', 'getTeam', []).then(function (members) {
-    			console.log('retour depuis odoo');
-    			team.members = members;
-    			return team.members;
-    		}, function (err) {
-    			team.members = ['1', '3', '5'];
-    			return team.members;
-    		});
-    	};
-    	team.set = function (memberList) {
-    		// will persist to odoo the new list
-		 	return jsonRpc.call('prodoo_team', 'persistTeam', [memberList]).then(function () {
-		        //do the changes
-		    }, function () {
-		        console.log('ya eu du grabbuge, on persist en local');
-		        team.members = memberList;
-
-		    });   		
-    	}
-    	team.get();
-    	return team;
+        team.get = function() {
+            //fetch the list from odoo
+            return jsonRpc.call('prodoo_team', 'getTeam', []).then(function (members) {
+                team.members = members;
+                return team.members;
+            });
+        };
+        team.set = function (memberList) {
+            // will persist to odoo the new list
+            return jsonRpc.call('prodoo_team', 'persistTeam', [memberList]).then(function () {
+                //do the changes
+                console.log('team set successfully');
+            });         
+        }
+        team.get();
+        return team;
     }];
 
     return this;
